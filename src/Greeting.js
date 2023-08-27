de909f2b6d0556bfc4183ad4c970592d9ba73552
Machiel <machielvanderwalt27@gmail.com>
2023-08-27 13:07:43 +0200

    Added tailwind

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Greeting(props) {
-  return <h1>Hello, {props.name}!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello, {props.name}!</h1>;
 }
 
 export default Greeting;
